b6aa76a4e107ec60ec62c4ea98d4c0a269d75aa0
jkhan-007 <imon.bot@gmail.com>
2019-03-23 07:32:28 +0000

    Added Stash and Apply support when creating feature branch

--- contextual/core/git_interactor.py
+++ contextual/core/git_interactor.py
@@ -23,9 +23,17 @@ def is_git_dir(working_dir):
 def create_feature_branch(working_dir, source_branch, new_branch):
     with Repo(working_dir) as repo:
         git = repo.git
-        stash_out = git.stash()
-        logging.debug("Git stash output")
+        stash_out = git.stash("push")
+        logging.debug("Git stash push")
         logging.debug(stash_out)
         repo.head.set_reference(repo.heads[source_branch])
         new_branch = repo.create_head(new_branch)
         new_branch.checkout()
+
+
+def apply_stash(working_dir):
+    with Repo(working_dir) as repo:
+        git = repo.git
+        stash_out = git.stash("pop")
+        logging.debug("Git stash pop")
+        logging.debug(stash_out)
--- contextual/ui/generated/feature_branch_dialog.py
+++ contextual/ui/generated/feature_branch_dialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'resources/ui/feature_branch_dialog.ui'
 #
-# Created by: PyQt5 UI code generator 5.11.3
+# Created by: PyQt5 UI code generator 5.10.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,16 +12,16 @@ class Ui_FeatureBranchDialog(object):
     def setupUi(self, FeatureBranchDialog):
         FeatureBranchDialog.setObjectName("FeatureBranchDialog")
         FeatureBranchDialog.setWindowModality(QtCore.Qt.WindowModal)
-        FeatureBranchDialog.resize(474, 268)
+        FeatureBranchDialog.resize(475, 318)
         FeatureBranchDialog.setModal(True)
         self.btn_create_branch = QtWidgets.QPushButton(FeatureBranchDialog)
-        self.btn_create_branch.setGeometry(QtCore.QRect(350, 230, 113, 31))
+        self.btn_create_branch.setGeometry(QtCore.QRect(350, 280, 113, 31))
         self.btn_create_branch.setObjectName("btn_create_branch")
         self.btn_cancel_create_branch = QtWidgets.QPushButton(FeatureBranchDialog)
-        self.btn_cancel_create_branch.setGeometry(QtCore.QRect(230, 230, 113, 30))
+        self.btn_cancel_create_branch.setGeometry(QtCore.QRect(230, 280, 113, 30))
         self.btn_cancel_create_branch.setObjectName("btn_cancel_create_branch")
         self.txt_main_branch = QtWidgets.QLineEdit(FeatureBranchDialog)
-        self.txt_main_branch.setGeometry(QtCore.QRect(140, 120, 321, 21))
+        self.txt_main_branch.setGeometry(QtCore.QRect(140, 148, 321, 21))
         self.txt_main_branch.setText("")
         self.txt_main_branch.setObjectName("txt_main_branch")
         self.label = QtWidgets.QLabel(FeatureBranchDialog)
@@ -41,20 +41,31 @@ class Ui_FeatureBranchDialog(object):
         self.label_4.setGeometry(QtCore.QRect(60, 80, 391, 16))
         self.label_4.setObjectName("label_4")
         self.label_5 = QtWidgets.QLabel(FeatureBranchDialog)
-        self.label_5.setGeometry(QtCore.QRect(20, 120, 111, 20))
+        self.label_5.setGeometry(QtCore.QRect(20, 148, 111, 20))
         self.label_5.setObjectName("label_5")
         self.label_6 = QtWidgets.QLabel(FeatureBranchDialog)
-        self.label_6.setGeometry(QtCore.QRect(20, 160, 111, 20))
+        self.label_6.setGeometry(QtCore.QRect(20, 188, 111, 20))
         self.label_6.setObjectName("label_6")
         self.txt_feature_branch = QtWidgets.QLineEdit(FeatureBranchDialog)
-        self.txt_feature_branch.setGeometry(QtCore.QRect(140, 160, 321, 21))
+        self.txt_feature_branch.setGeometry(QtCore.QRect(140, 188, 321, 21))
         self.txt_feature_branch.setObjectName("txt_feature_branch")
         self.lbl_error = QtWidgets.QLabel(FeatureBranchDialog)
-        self.lbl_error.setGeometry(QtCore.QRect(20, 200, 441, 16))
+        self.lbl_error.setGeometry(QtCore.QRect(20, 258, 441, 16))
         self.lbl_error.setObjectName("lbl_error")
+        self.chk_apply_stash = QtWidgets.QCheckBox(FeatureBranchDialog)
+        self.chk_apply_stash.setGeometry(QtCore.QRect(140, 228, 221, 20))
+        self.chk_apply_stash.setChecked(True)
+        self.chk_apply_stash.setObjectName("chk_apply_stash")
+        self.label_7 = QtWidgets.QLabel(FeatureBranchDialog)
+        self.label_7.setGeometry(QtCore.QRect(60, 99, 391, 16))
+        self.label_7.setObjectName("label_7")
 
         self.retranslateUi(FeatureBranchDialog)
         QtCore.QMetaObject.connectSlotsByName(FeatureBranchDialog)
+        FeatureBranchDialog.setTabOrder(self.txt_main_branch, self.txt_feature_branch)
+        FeatureBranchDialog.setTabOrder(self.txt_feature_branch, self.chk_apply_stash)
+        FeatureBranchDialog.setTabOrder(self.chk_apply_stash, self.btn_cancel_create_branch)
+        FeatureBranchDialog.setTabOrder(self.btn_cancel_create_branch, self.btn_create_branch)
 
     def retranslateUi(self, FeatureBranchDialog):
         _translate = QtCore.QCoreApplication.translate
@@ -63,9 +74,11 @@ class Ui_FeatureBranchDialog(object):
         self.btn_cancel_create_branch.setText(_translate("FeatureBranchDialog", "Cancel"))
         self.label.setText(_translate("FeatureBranchDialog", "Creates new feature branch"))
         self.label_2.setText(_translate("FeatureBranchDialog", "- Stash all changes in the current branch"))
-        self.label_3.setText(_translate("FeatureBranchDialog", "- Checks out main branch (As shown below and can be changed)"))
-        self.label_4.setText(_translate("FeatureBranchDialog", "- Creates a check out new branch"))
+        self.label_3.setText(_translate("FeatureBranchDialog", "- Checks out main branch (As shown and can be changed)"))
+        self.label_4.setText(_translate("FeatureBranchDialog", "- Creates and checks out new branch"))
         self.label_5.setText(_translate("FeatureBranchDialog", "Main branch"))
         self.label_6.setText(_translate("FeatureBranchDialog", "Feature branch"))
         self.lbl_error.setText(_translate("FeatureBranchDialog", "Error message"))
+        self.chk_apply_stash.setText(_translate("FeatureBranchDialog", "Apply Stash on new branch"))
+        self.label_7.setText(_translate("FeatureBranchDialog", "- Apply Stash (If selected)"))
 
